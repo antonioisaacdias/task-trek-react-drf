e9e74152f38102cb1f18b34b5dd74d96ff0332bf
Antonio Dias <antonioisaacvd@gmail.com>
2025-04-01 23:28:02 -0300

    feat: Implementa view de login.

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from django.contrib.auth import authenticate, login, logout
+from knox.models import AuthToken
 
-@APIView(['POST'])
-@permission_cla
+@api_view(['POST'])
 def login_view(request):
     if request.method == 'POST':
         username = request.data.get('username')
@@ -14,7 +14,17 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
+            token = AuthToken.objects.create(user=user)
+            print(token)
+            return Response({
+                'user': {
+                    'id': user.id,
+                    'username': user.username
+                },
+                'token': token[1],
+            })
+
+            
         else:
             return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
     return Response({'error': 'Method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
